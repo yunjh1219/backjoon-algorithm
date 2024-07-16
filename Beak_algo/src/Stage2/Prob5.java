720ebcbc869b22a7b849b7b3e992e509ef98a3b1
yunsi1219 <orenjh1@naver.com>
2024-07-16 23:23:44 +0900

    Day 4(1)

--- Beak_algo/src/Stage2/Prob5.java
+++ Beak_algo/src/Stage2/Prob5.java
@@ -7,11 +7,23 @@ public class Prob5 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		int H = sc.nextInt();
-		int M = sc.nextInt();
-		
-	    System.out.println(H%24);
-	    
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+        int newH, newM;
+	   
+		if(m>=45) {
+			newH = h;
+			newM = m-45;
+		} else {
+			newH = h-1;
+			newM = m+15;
+			if (newH < 0) {
+				newH=23;
+			}
+		} 
+		System.out.println(newH+" "+newM);
 		
 	}
 }
+
+
