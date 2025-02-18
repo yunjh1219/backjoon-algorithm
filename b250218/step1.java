7042cd673916972dada0dbc1e9a0a71a275a5b99
[yunjh1219] <[orenjh1219@gmail.com]>
2025-02-18 22:27:58 +0900

    25-02-18

---
+++ b250218/step1.java
@@ -0,0 +1,7 @@
+package b250218;  // 패키지 선언
+
+public class step1 {  // 클래스 정의
+    public static void main(String[] args) {  // main 메서드 정의
+        System.out.print("Hello World!");  // 콘솔에 "Hello World!" 출력
+    }
+}
---
+++ b250218/step2.java
@@ -0,0 +1,12 @@
+package b250218;
+
+import java.util.Scanner;
+
+public class step2 {
+	  public static void main(String[]args){
+		     Scanner scan = new Scanner(System.in);
+		     int A = scan.nextInt();
+		     int B = scan.nextInt();
+		     System.out.print(A+B);
+		     }
+}
