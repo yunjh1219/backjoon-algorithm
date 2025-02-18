30f16f6759f9294c75ab0f253fc34a7262bd704b
[yunjh1219] <[orenjh1219@gmail.com]>
2025-02-18 22:34:19 +0900

    250218 나누기

---
+++ b250218/step5.java
@@ -0,0 +1,13 @@
+package b250218;
+
+import java.util.Scanner;
+
+public class step5 {
+	public static void main(String[]args) {
+		Scanner scan = new Scanner(System.in);
+	      double A = scan.nextDouble();  // 실수 입력받기
+	      double B = scan.nextDouble();  // 실수 입력받기
+		System.out.println(A/B);
+	}
+
+}
